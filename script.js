34d6cee3f5450f648ce0fbab7745f7f00e5d9cce
EdgarGrcs <edgar.gricius2@gmail.com>
2021-11-27 00:05:12 +0100

    added some variables and if statements

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function computerPlay() {
 
+    let rock = "rock";
+    let paper = "paper";
+    let scissors = "scissors";
 
+    let number = Math.floor(Math.random());
 
-    return random;
+    if (number == 0) {
+        return rock;
+    }
+
+    if (number == 1) {
+        return paper;
+    }
+    return scissors;
 }
